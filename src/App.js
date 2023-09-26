ff699eb592c3f3efa6861d839645be2c02506394
rod773 <104315743+rod773@users.noreply.github.com>
2023-09-26 18:14:53 +0200

    login and sign componets

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
+import Login from "./components/pages/Auth/Login";
+import SignUp from "./components/pages/Auth/SignUp";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Router>
         <Routes>
           <Route exaxct path="/" element={<Home />} />
+          <Route exaxct path="/login" element={<Login />} />
+          <Route exaxct path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
     </div>
--- src/components/pages/Auth/SignUp.js
+++ src/components/pages/Auth/SignUp.js
@@ -1,5 +1,41 @@
-import React from "react";
+import "./Auth.css";
 
-export default function SignUp() {
-  return <div>SignUp</div>;
+function SignUp() {
+  return (
+    <div className="text-center div">
+      <form className="form-signin">
+        <h1 className="h3 mb-3 font-weight-normal">Please sign up</h1>
+        <label htmlFor="inputEmail" className="sr-only">
+          Email address
+        </label>
+        <input
+          type="email"
+          id="inputEmail"
+          className="form-control"
+          placeholder="Email address"
+          required
+        />
+        <label htmlFor="inputPassword" className="sr-only">
+          Password
+        </label>
+        <input
+          type="password"
+          id="inputPassword"
+          className="form-control"
+          placeholder="Password"
+          required
+        />
+        <div className="checkbox mb-3">
+          <label>
+            <input type="checkbox" defaultValue="remember-me" /> Remember me
+          </label>
+        </div>
+        <button className="btn btn-lg btn-primary btn-block" type="submit">
+          Sign up
+        </button>
+      </form>
+    </div>
+  );
 }
+
+export default SignUp;
